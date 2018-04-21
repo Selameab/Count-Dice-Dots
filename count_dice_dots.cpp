6eba2ef710692df4e61c27a5e2cbfd7d780569b1
SS <email@selameab.com>
2018-04-21 19:48:24 +0300

    Read, display and write image

--- count_dice_dots.cpp
+++ count_dice_dots.cpp
@@ -10,6 +10,26 @@ using namespace cv;
 using namespace std;
 
 int main(int argc, char* argv[]) {
+  // Check args
+  if( argc != 3)
+  {
+    cout <<"Usage: count_dice_dots input_image output_image" << endl;
+    return -1;
+  }
+
+  // Variables
+  Mat img, threshImg;
+
+  // Read image
+  img = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+
+  // Display image
+  namedWindow(argv[1], WINDOW_AUTOSIZE);
+  imshow(argv[1], img);
+  waitKey(0);
+
+  // Save to file
+  imwrite(argv[2], img);
 
   return 0;
 }
