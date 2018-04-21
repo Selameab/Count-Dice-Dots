e139f261ab1fb4d41b81ddac48aaf99e2945f84f
SS <email@selameab.com>
2018-04-21 19:24:25 +0300

    Initial

---
+++ count_dice_dots.cpp
@@ -0,0 +1,15 @@
+/*
+ * File:   count_dice_dots.cpp
+ * Author: Selameab
+ */
+
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+  return 0;
+}
